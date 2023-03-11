ea2eefb802e6a4fa55c2a7bad2beae2ba3da6737
kamal98ETH <bestkacm@gmail.com>
2023-03-11 17:57:03 +0100

    fix: routing

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,18 @@ import './App.css';
 import React from 'react';
 import Home from './home';
 import Game from './game';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 
 const App: React.FC = () => {
 
   return (
-    <BrowserRouter basename="/tic-tac-toe-client">
+    <HashRouter>
       <Routes>
         <Route path='/' element={<Home />}></Route>
         <Route path='/game' element={<Game />}>
         </Route>
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
